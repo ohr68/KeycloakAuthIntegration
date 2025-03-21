151cc2b9768f4b251b4bebbf1a9c9892558b7c1f
Otávio Odenik <otavio.odenik@gmail.com>
2025-03-21 17:06:10 -0300

    adds project setup

---
+++ src/KeycloakAuthIntegration.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+
+namespace WebApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureKeycloak(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .ConfigureKeycloakAuth(configuration)
+            .ConfigureKeycloakAuthorization(configuration);
+        
+        return services;
+    }
+
+    private static IServiceCollection ConfigureKeycloakAuth(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddKeycloakWebApiAuthentication(configuration);
+        
+        return services;
+    }
+
+    private static IServiceCollection ConfigureKeycloakAuthorization(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddKeycloakAuthorization(configuration);
+        
+        return services;
+    }
+}
---
+++ src/KeycloakAuthIntegration.WebApi/Program.cs
@@ -0,0 +1,59 @@
+using KeycloakAuthIntegration.IoC.HealthChecks;
+using KeycloakAuthIntegration.IoC.Logging;
+using Serilog;
+using WebApi.Extensions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Log.Information("Starting web application");
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.AddDefaultLogging();
+
+            builder.Services.AddProblemDetails(options =>
+                options.CustomizeProblemDetails = ctx =>
+                {
+                    ctx.ProblemDetails.Extensions.Add("trace-id", ctx.HttpContext.TraceIdentifier);
+                    ctx.ProblemDetails.Extensions.Add("instance",
+                        $"{ctx.HttpContext.Request.Method} {ctx.HttpContext.Request.Path}");
+                });
+            
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.AddBasicHealthChecks();
+            builder.Services.ConfigureKeycloak(builder.Configuration);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBasicHealthChecks();
+            app.MapControllers().RequireAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.Run();
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+            Console.WriteLine($"Critical error: {ex.Message}");
+            Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
+            Console.WriteLine(ex.StackTrace);
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
